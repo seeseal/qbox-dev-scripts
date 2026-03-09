1c94cc23988ef93fdda8339d0385616f2a359ef8
Cecil Mathew <cecilsm901@gmail.com>
2026-03-09 15:16:44 +0530

    Update server.lua

--- discord_webhook/server.lua
+++ discord_webhook/server.lua
@@ -6,12 +6,12 @@
 -- Add your Discord webhook URLs here
 -- One webhook per channel in your Discord server
 local webhooks = {
-    general    = "",
-    dealership = "",
-    turf       = "",
-    housing    = "",
-    chop       = "",
-    tickets    = "",
+    general    = "https://discord.com/api/webhooks/1480500600721375232/zHMvSvKAPi9T_tfhthKRzzDKfBQFSULCRNF5P2Y7wQnadPTGlsHmG0PwTWpSTxhWji-B",
+    dealership = "https://discord.com/api/webhooks/1480500804556423211/n-xV5f9Y-GiaLv_KNylvCI46XUK7H_hcaNvULxYl_ZwoyE9SDD6XZEFYIXJfNgzuvxm7",
+    turf       = "https://discord.com/api/webhooks/1480501260523405398/ChJQQurrJuK_97r5xI8O4FJEod3uIU1ZDpwriiOQ3JMjkYTsutdtVZ5ObyxPD1jMzs1C",
+    housing    = "https://discord.com/api/webhooks/1480501379142520914/WXkhVsEV9_eCP341JJkIkvkJkHJteC8sCKm_XQ6W0hG_TEfAJncibt9vW3hpZpGZSonq",
+    chop       = "https://discord.com/api/webhooks/1480501483047882875/0FenPoJdRLeigJ7yCsCpQrElajUKPIqc6jiHdF2loRaWjYChv-Lw62BC6fRHzkAZ-MF7",
+    tickets    = "https://discord.com/api/webhooks/1480501559010922641/ThDXwdXfiymkAK4GExXP49mJx2h5MyB2iX9tVWWsEHua5D81CtEYWdHgWsMnPMqNyE5R ",
 }
 
 -- Colors (decimal) for embed sidebar
